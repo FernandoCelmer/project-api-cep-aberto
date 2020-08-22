4c4f8097fb8c8f3bef42ebff48154c71281560a9
Fernando Celmer <email@fernandocelmer.com>
2020-08-22 15:14:13 -0300

    Template

---
+++ template/content-top.php
@@ -0,0 +1,6 @@
+<section class="jumbotron text-center">
+    <div class="container">
+      <h1><a href="https://github.com/FernandoCelmer/indexoffy-api-cep-aberto">[indexoffy-api-cep-aberto]</a></h1>
+      <p>Basic query in the CEP-Aberto project database</p>
+    </div>
+  </section>
---
+++ template/design.php
@@ -0,0 +1,5 @@
+    <!-- Bootstrap CSS -->
+    <link href="packages/bootstrap/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Project CSS -->
+    <link href="packages/css/style.css" rel="stylesheet">
