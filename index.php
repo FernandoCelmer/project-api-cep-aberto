4f20660ae405c44195ad4d2e34f85e352d055264
Fernando Celmer <email@fernandocelmer.com>
2020-08-22 02:36:24 -0300

    Página Inicial

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
+require_once "api/config/config.php";
+require_once "api/ServiceAPI.php";
 
+$array = [
+    "estado" => "RS",
+    "cidade" => "Flores da Cunha",
+  ];
+
+$api = new ServiceAPI(API_KEY);
+echo $api->getAddress($array);
 
 ?>
